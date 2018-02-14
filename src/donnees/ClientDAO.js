8dd92eb858aee210e5213075f8ec8360f301cb97
GiovanniScarpellino <scarpellino.giovanni@gmail.com>
2018-02-15 00:07:01 +0100

    Modification de la recuperation d'un client avec son numéro de telephone

--- src/donnees/ClientDAO.js
+++ src/donnees/ClientDAO.js
@@ -1,13 +1,17 @@
 exports.ajouterClient = function (client, callback) {
     global.bdd.collection("client").insert(client, function (erreur, resultat) {
-        if (erreur) callback({resultat: -1});
-        callback({resultat: 1});
+        if (erreur) callback({ resultat: -1 });
+        callback({ resultat: 1 });
     });
 };
 
 exports.recupererClient = function (parametres, callback) {
-    global.bdd.collection("client").findOne({telephone: parametres.telephone}, function (erreur, resultat) {
-        if (erreur) callback({resultat: -1});
-        callback({resultat: 1});
+    global.bdd.collection("client").findOne({ telephone: parametres.telephone }, function (erreur, resultat) {
+        try {
+            if (resultat.motDePasse != parametres.motDePasse) callback({ resultat: -1 })
+            callback({ resultat: 1 })
+        } catch (e) {
+            callback({ resultat: -1 });
+        }
     });
 };
