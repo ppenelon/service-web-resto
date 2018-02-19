c717f676fd9c9b7278c441690215f70ad887a1d7
ppenelon <penelon.pierre@gmail.com>
2018-02-20 00:22:52 +0100

    Formattage de l'affichage de l'heure lors de la récupération des réservations client & restaurant

--- src/donnees/ReservationDAO.js
+++ src/donnees/ReservationDAO.js
@@ -1,5 +1,5 @@
 exports.recupererReservationClientAvecToken = function(tokenClient, callback){
-    var requete = `SELECT DATE(reservation.date) AS date, TIME(reservation.date) AS heure, reservation.nombrePersonnes AS nombrePersonnes, restaurant.nom AS nomRestaurant, restaurant.adresse AS adresseRestaurant
+    var requete = `SELECT DATE(reservation.date) AS date, DATE_FORMAT(reservation.date, "%H:%i") AS heure, reservation.nombrePersonnes AS nombrePersonnes, restaurant.nom AS nomRestaurant, restaurant.adresse AS adresseRestaurant
                    FROM reservation 
                    JOIN client ON reservation.idClient = client.idClient
                    JOIN restaurant ON restaurant.idRestaurant = reservation.idRestaurant
@@ -23,7 +23,7 @@ exports.recupererReservationClientAvecToken = function(tokenClient, callback){
 }
 
 exports.recupererReservationRestaurantAvecToken = function(tokenRestaurant, callback){
-    var requete = `SELECT TIME(reservation.date) AS heure, reservation.nombrePersonnes AS nombrePersonnes, CONCAT(client.nom,' ',client.prenom) AS nomClient, client.telephone AS telephoneClient
+    var requete = `SELECT DATE_FORMAT(reservation.date, "%H:%i") AS heure, reservation.nombrePersonnes AS nombrePersonnes, CONCAT(client.nom,' ',client.prenom) AS nomClient, client.telephone AS telephoneClient
                    FROM reservation
                    JOIN restaurant ON reservation.idRestaurant = restaurant.idRestaurant
                    JOIN client ON client.idClient = reservation.idClient
