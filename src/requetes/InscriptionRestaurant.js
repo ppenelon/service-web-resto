be8008d6f33cec09cd5ad24f477794509ac86353
lucas <lucas.paulin@hotmail.fr>
2018-02-22 13:34:03 +0100

    "Sécurisation" de l'api pour l'inscription d'un restaurateur

--- src/requetes/InscriptionRestaurant.js
+++ src/requetes/InscriptionRestaurant.js
@@ -6,6 +6,29 @@ const regex = require('../assistants/Regex');
 const tokenAssistant = require('../assistants/Token');
 
 module.exports = function(parametres, callback) {
+    // Vérification des paramètres
+
+    if(
+        !parametres.hasOwnProperty("nom") ||
+        !parametres.hasOwnProperty("adresse") ||
+        !parametres.hasOwnProperty("telephone") ||
+        !parametres.hasOwnProperty("mail") ||
+        !parametres.hasOwnProperty("descripion") ||
+        !parametres.hasOwnProperty("motDePasse")
+    ) {
+        throw new Error("Parametres manquants");
+    }
+
+    if(
+        parametres.nom.length > 150 ||
+        parametres.adresse > 150 ||
+        !regex.telephone(parametres.telephone) ||
+        !regex.mail(parametres.mail) ||
+        !regex.motDePasse(parametres.motDePasse)
+    ){
+        throw new Error("Mauvais format des paramètres");
+    }
+
     //création du nouveau restaurant
     var resto = restoDAO.creeRestaurantAvecParametres(parametres);
 
