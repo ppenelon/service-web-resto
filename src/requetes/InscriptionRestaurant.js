3350321b25993c4d78702f383d9d092e22c39e88
GiovanniScarpellino <31245981+GiovanniScarpellino@users.noreply.github.com>
2018-02-23 18:38:32 +0100

    Correction de la valeur de length pour l'adresse pour l'inscription Restaurant

--- src/requetes/InscriptionRestaurant.js
+++ src/requetes/InscriptionRestaurant.js
@@ -1,43 +1,39 @@
-const resto = require('../modeles/Restaurant');
+const resto = require("../modeles/Restaurant");
 
-const restoDAO = require('../donnees/RestaurantDAO');
+const restoDAO = require("../donnees/RestaurantDAO");
 
-const regex = require('../assistants/Regex');
-const tokenAssistant = require('../assistants/Token');
-const convertirAdresse = require('../assistants/ConvertirAdresse');
+const regex = require("../assistants/Regex");
+const tokenAssistant = require("../assistants/Token");
+const convertirAdresse = require("../assistants/ConvertirAdresse");
 
 module.exports = function(parametres, callback) {
-    // Vérification des paramètres
+  // Vérification des paramètres
 
-    if(
-        !parametres.hasOwnProperty("nom") ||
-        !parametres.hasOwnProperty("adresse") ||
-        !parametres.hasOwnProperty("telephone") ||
-        !parametres.hasOwnProperty("mail") ||
-        !parametres.hasOwnProperty("description") ||
-        !parametres.hasOwnProperty("motDePasse")
-    ) {
-        throw new Error("Parametres manquants");
-    }
+  if (!parametres.hasOwnProperty("nom") ||
+    !parametres.hasOwnProperty("adresse") ||
+    !parametres.hasOwnProperty("telephone") ||
+    !parametres.hasOwnProperty("mail") ||
+    !parametres.hasOwnProperty("description") ||
+    !parametres.hasOwnProperty("motDePasse")) {
+    throw new Error("Parametres manquants");
+  }
 
-    if(
-        parametres.nom.length > 150 ||
-        parametres.adresse.length > 150 ||
-        !regex.telephone(parametres.telephone) ||
-        !regex.mail(parametres.mail) ||
-        !regex.motDePasse(parametres.motDePasse)
-    ){
-        throw new Error("Mauvais format des paramètres");
-    }
+  if (parametres.nom.length > 150 ||
+    parametres.adresse.length > 300 ||
+    !regex.telephone(parametres.telephone) ||
+    !regex.mail(parametres.mail) ||
+    !regex.motDePasse(parametres.motDePasse)) {
+    throw new Error("Mauvais format des paramètres");
+  }
 
-    convertirAdresse.convertir(parametres.adresse, function(position){
-        parametres.latitude = position.latitude;
-        parametres.longitude = position.longitude;
-        
-        var resto = restoDAO.creeRestaurantAvecParametres(parametres);
+  convertirAdresse.convertir(parametres.adresse, function(position) {
+    parametres.latitude = position.latitude;
+    parametres.longitude = position.longitude;
 
-        restoDAO.ajouterRestaurant(resto, function(resultat) {
-            callback(resultat);
-        });
+    var resto = restoDAO.creeRestaurantAvecParametres(parametres);
+
+    restoDAO.ajouterRestaurant(resto, function(resultat) {
+      callback(resultat);
     });
+  });
 };
