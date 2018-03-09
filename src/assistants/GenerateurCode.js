b84986241af05b9dcda795ee2ff1e7bd1f7c4ec9
ppenelon <penelon.pierre@gmail.com>
2018-03-09 18:00:44 +0100

    Ajout de l'assistant qui calcule un code utilisateur pour le système de fidélité via son ID

---
+++ src/assistants/GenerateurCode.js
@@ -0,0 +1,32 @@
+//Alphabet disponible pour convertir l'ID du client
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+//Fonction qui génère un code client pour le système de fidélité
+//Retourne une chaine de la forme O-O-O-O (Où O est un élément de l'alphabet ci-dessus et - est un chiffre)
+exports.genererCodeClient = function(idClient){
+    //On décrémente l'ID du client pour commencer la conversion à 1 au lieu de 0
+    idClient--;
+    //On initialise la chaine finale qu'on retournera ainsi qu'une variable qui contiendra la lettre de l'alphabet associée au chiffre convertit
+    var chaineFinale = "";
+    var lettre;
+    //On calcule X (O-O-O-X)
+    lettre = alphabet.charAt(id % alphabet.length);
+    chaineFinale = lettre + chaineFinale;
+    //On ajoute un chiffre aléatoire de 0 à 9 à l'endroit _ (O-O-O_O)
+    chaineFinale = Math.round(Math.random() * 9) + chaineFinale;
+    //On calcule X (O-O-X-O)
+    lettre = alphabet.charAt(Math.floor(id / alphabet.length) % alphabet.length);
+    chaineFinale = lettre + chaineFinale;
+    //On ajoute un chiffre aléatoire de 0 à 9 à l'endroit _ (O-O_O-O)
+    chaineFinale = Math.round(Math.random() * 9) + chaineFinale;
+    //On calcule X (O-X-O-O)
+    lettre = alphabet.charAt(Math.floor(id / (alphabet.length * alphabet.length)) % alphabet.length);
+    chaineFinale = lettre + chaineFinale;
+    //On ajoute un chiffre aléatoire de 0 à 9 à l'endroit _ (O_O-O-O)
+    chaineFinale = Math.round(Math.random() * 9) + chaineFinale;
+    //On calcule X (X-O-O-O)
+    lettre = alphabet.charAt(Math.floor(id / (alphabet.length * alphabet.length * alphabet.length)) % alphabet.length);
+    chaineFinale = lettre + chaineFinale;
+    //On retourne le résultat
+    return chaineFinale;
+}
